b6f82f241ee318b896571cc253948e5f327af95f
Franco Melcon <fmelcon@live.com>
2021-07-14 19:30:08 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hola</h1>
-    </div>
+    <Fragment>
+      <Header titulo="Clima React App" />
+    </Fragment>
   );
 }
 
